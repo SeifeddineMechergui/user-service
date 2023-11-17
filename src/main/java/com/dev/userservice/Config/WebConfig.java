4aa6b375b03f4a86b192464865848d30bdaa033c
SeifeddineMechergui <saifmechergui2222@gmail.com>
2023-11-17 04:44:39 +0100

    Simple crud

---
+++ src/main/java/com/dev/userservice/Config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.dev.userservice.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // Add the origin of your Angular app
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true);
+    }
+}
