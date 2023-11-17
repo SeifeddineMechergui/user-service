01e24417ff2c64a90a09af279dfa3ceac1f9b2c4
SeifeddineMechergui <saifmechergui2222@gmail.com>
2023-11-17 03:14:35 +0100

    Simple crud

--- src/main/java/com/dev/userservice/web/UserController.java
+++ src/main/java/com/dev/userservice/web/UserController.java
@@ -3,9 +3,7 @@ package com.dev.userservice.web;
 import com.dev.userservice.dao.UserRepository;
 import com.dev.userservice.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +17,28 @@ public class UserController {
     private List<User> getusers(){
         return userRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    private User getById(@PathVariable long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @PostMapping
+    private User saveUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+    @DeleteMapping("/{id}")
+    private void deleteUser(@PathVariable Long id){
+        userRepository.deleteById(id);
+    }
+
+    /*@PutMapping("/{id}")
+    private User updateUser(@RequestBody User newUser,@PathVariable long id){
+       User ExUser=userRepository.findById(id).get();
+       ExUser=newUser;
+       return userRepository.save(ExUser);
+    }
+*/
 
 
 }
